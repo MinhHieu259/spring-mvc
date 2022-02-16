53ddd83fbbcd2b375eb76ea98a41f847e9852ee9
MinhHieu259 <mhieu7252@gmail.com>
2022-02-16 10:28:29 +0700

    add bnbcnv

---
+++ src/main/java/com/minhhieu/dto/MyUser.java
@@ -0,0 +1,16 @@
+package com.minhhieu.dto;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class MyUser extends User {
+
+	public MyUser(String username, String password, boolean enabled, boolean accountNonExpired,
+			boolean credentialsNonExpired, boolean accountNonLocked,
+			Collection<? extends GrantedAuthority> authorities) {
+		super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+	}
+
+}
--- src/main/java/com/minhhieu/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/minhhieu/service/impl/CustomUserDetailsService.java
@@ -1,12 +1,19 @@
 package com.minhhieu.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.minhhieu.constant.SystemConstant;
+import com.minhhieu.entity.RoleEntity;
 import com.minhhieu.entity.UserEntity;
 import com.minhhieu.repository.UserRepository;
 
@@ -21,6 +28,13 @@ public class CustomUserDetailsService implements UserDetailsService{
 		if(userEntity == null) {
 			throw new UsernameNotFoundException("User not found");
 		}
+		
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		for(RoleEntity role : userEntity.getRoles()) {
+			authorities.add(new SimpleGrantedAuthority(role.getCode()));
+		}
+		// Put  thông tin vào security lưu thông tin người dùng
+		User user = new User(userEntity.getUserName(), userEntity.getPassword(), true, true, true, true, authorities);
 		return null;
 	}
 
